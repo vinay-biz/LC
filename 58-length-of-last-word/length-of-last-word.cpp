bfc2d18100490c811d0ad2d0e7c53bc4de7a5736
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-01 11:39:35 +0530

    Time: 5 ms (47.87%) | Memory: 7.7 MB (20.26%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int l=0, i = s.length()-1;
-
-        while(i>=0 && s[i]==' ') i--;
-      
-        while(i>=0 && s[i]!=' ')
-        {
-            i--;
-            l++;
+        int length = 0;
+        bool counting = false;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                counting = true;
+                length++;
+            }
+            else if (counting) {
+                break;
+            }
         }
         
-        return l;
+        return length;
     }
 };
