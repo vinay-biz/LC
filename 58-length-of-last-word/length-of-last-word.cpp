442c1c0f947459b23968eed2d4213813b35fa515
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-01 11:34:37 +0530

    Time: 4 ms (47.87%) | Memory: 7.7 MB (20.26%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        bool flag = false; //To indicate first letter encounter
+        int length=0;
+
+        for(int i=s.length()-1 ; i>=0; i--) //Lets find first character that is not a whitespace
+        {
+            if(s[i]==' ') continue;
+
+            else
+            {
+                while(i>=0 && s[i] != ' ')
+                {
+                    length++;
+                    i--;
+                }
+                break;
+            }
+        }
+        return length;
+    }
+};
