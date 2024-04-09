0a44e19d0cf7022b87757e34fa5ff96faf0f84ea
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-09 23:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (91.31%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -4,13 +4,13 @@ public:
         int ans = 0;
         for(int i=0; i<tickets.size(); i++)
         {
-            if(i == k) continue;
+            if(i == k) continue; // skip the actual tickets count
 
-            if(tickets[i]<tickets[k]) ans += tickets[i];
+            if(tickets[i]<tickets[k]) ans += tickets[i]; //we will def buy all tickets
 
-            if(tickets[i]>=tickets[k] && i>k) ans += tickets[k] - 1;
+            if(tickets[i]>=tickets[k] && i>k) ans += tickets[k] - 1; //we will only buy tickets till k is not zero so k-1
 
-            if(tickets[i]>=tickets[k] && i<k) ans+= tickets[k];
+            if(tickets[i]>=tickets[k] && i<k) ans+= tickets[k];//we will def buy k tickets
         }
 
         ans+= tickets[k];
