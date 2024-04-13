a0625db48405543f4db04263037d66163fbd9cda
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-13 15:40:56 +0530

    Time: 3 ms (45.09%) | Memory: 7.4 MB (60.55%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,13 +3,9 @@ public:
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length()) return false; //Two string can not be rotated then
 
-        for(int i = 0;i <= s.size(); i++) //<= , if not, the condition of s == goal or last permu will be skipped 
-        {
-            s = s + s[0]; //add first element to back
-            s.erase(0,1); //remove first element
-            if(s==goal) return true; //check
-        }
+        string ans = s + s;
 
-        return false;
+        if(ans.find(goal) == string::npos) return false;
+        return true;
     }
 };
