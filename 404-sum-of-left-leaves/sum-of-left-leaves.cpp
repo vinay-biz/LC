d4d0346fab8c1e1afcceb905e7672269f70e60a7
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-14 19:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (81.87%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -5,7 +5,7 @@ public:
         if(root == NULL) return 0;
 
         int sum = 0;
-        if(root->left!=NULL && root->left->left==NULL && root->left->right == NULL) sum = sum + root->left->val;
+        if(root->left!=NULL && root->left->left==NULL && root->left->right == NULL) sum += root->left->val;
 
         return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
     }
