3fc3e800bc89aa89b4ce449c01f419c252146821
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-15 12:12:28 +0530

    Time: 16 ms (31.29%) | Memory: 9.8 MB (33.82%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int left=0, right=0;
-        unordered_map <char,int> mp,backup;
+        int left=0, right=0; //set left and right pointer
+        unordered_map <char,int> mp;
 
-        for(int i=0; i<s1.size(); i++) 
-        {
-            mp[s1[i]]++;
-            backup[s1[i]]++;
-        }
+        for(int i=0; i<s1.size(); i++) mp[s1[i]]++;
 
         while(left < s2.size() && right < s2.size())
         {
             if(mp.find(s2[right])==mp.end()) //incase the element is not a part
             {
+                while(left < right) //make the hashmap right again
+                {
+                    mp[s2[left]]++;
+                    left++;
+                }
                 left = right + 1;
                 right = right + 1;
-                mp = backup;
             }
 
-            else
+            else //incase element is a part 
             {
-                if(mp[s2[right]] > 0)
+                if(mp[s2[right]] > 0) //if we haven't gone to minus value yet
                 {
                     mp[s2[right]]--;
-                    if((right-left+1) == s1.length()) return true;
-                    right++;
+                    if((right-left+1) == s1.length()) return true; //maybe we're just done
+                    right++; //otherwise next element please
                 }
 
-                if(mp[s2[right]] == 0)
+                if(mp[s2[right]] == 0) //incase the values are gonna go to negitive
                 {
-                    while(mp[s2[right]] == 0)
+                    while(mp[s2[right]] == 0) //lets reduce left till it can be poititve again
                     {
                         mp[s2[left]]++;
                         left++;
-                    }
+                    } //remember no shifting right yet, we have to still check the new string
                 }
             }
         }
 
-        return false;
+        return false; //No luck :(
 
 
     }
