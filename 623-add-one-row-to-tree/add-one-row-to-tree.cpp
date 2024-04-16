f7d93817402c0c08c31c336c13b10d7e2118c593
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-04-16 12:32:22 +0530

    Time: 7 ms (95.21%) | Memory: 24.7 MB (14.90%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -37,8 +37,8 @@ public:
 
                 if(level == depth) return root;
 
-                if(node->left) q.push({node->left,level+1});
-                if(node->right) q.push({node->right,level+1});
+                if(node->left != NULL) q.push({node->left,level+1});
+                if(node->right != NULL) q.push({node->right,level+1});
         }
 
         return root;
