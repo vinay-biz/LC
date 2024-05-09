da9abb104958f92fb68f0700fe06e2ef474b4d04
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-05-09 22:03:13 +0530

    Time: 94 ms (99.73%) | Memory: 107.3 MB (32.91%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+
         sort(happiness.begin(),happiness.end(),greater<int>());
         int i=0;
         long long ans = 0;
