30d51b76ce9c8ecfb39f9699fa32ed0fd151e647
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-05-11 22:37:02 +0530

    Time: 123 ms (7.58%) | Memory: 130.4 MB (97.78%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int fuelReq = 0; //To tell if we can ever reach
+        int tank = 0; //To check where it becomes postitve, and dosen't go -ve again
+        int start = 0;
+
+        for(int i=0; i<gas.size(); i++)
+        {   
+            fuelReq += gas[i] - cost[i]; //Update current gas/req ratio
+            tank += gas[i] - cost[i]; //Do the same
+            if(tank < 0) //if ratio is neg, the next one might be the ans, not this one though
+            {
+                tank = 0;
+                start = i+1;
+            }
+        }
+
+        return (fuelReq<0)?-1:start;
+    }
+};
