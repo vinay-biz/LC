cd64effa5025f69ab68b9e12001c73adec72e267
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-05-17 14:48:01 +0530

    Time: 24 ms (51.73%) | Memory: 32.2 MB (36.82%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -11,22 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode* findLeftmost(TreeNode* root)
-    {
+    TreeNode* findLeftmost(TreeNode* root) {
         if(root->left == NULL) return root;
-
         return findLeftmost(root->left);
     }
 
-    TreeNode* helper(TreeNode* root)
-    {
+    TreeNode* helper(TreeNode* root){
         if(root->left == NULL) return root->right;
-
         if(root->right == NULL) return root->left;
 
-        TreeNode* leftmost = findLeftmost(root->right);
-        
-        leftmost->left = root->left;
+        TreeNode* leftmost = findLeftmost(root->right);//find the smallest number on right tree
+        leftmost->left = root->left; 
 
         return root->right;
     }
@@ -35,31 +30,19 @@ public:
         if(root == NULL) return root; 
 
         if(root->val == key) return helper(root); //Incase starting node is target
-         
         TreeNode* ans = root;
 
-        while(root) //Search for key and apply the deletion function
-        {
-            if(key < root->val) //will be on the left side then
-            {
-                if(root->left != NULL && root->left->val == key)
-                {
-                    root->left = helper(root->left);
-                }
-
+        while(root != NULL) { //Search for key and apply the deletion function
+            if(key < root->val) { //will be on the left side then
+                if(root->left != NULL && root->left->val == key) root->left = helper(root->left);
                 else root = root->left;
             }
 
-            else 
-            {
-                if(root->right && root->right->val == key)
-                {
-                    root->right = helper(root->right);
-                }
+            else {
+                if(root->right != NULL && root->right->val == key) root->right = helper(root->right);
                 else root = root->right;
             }
         }
-
         return ans;
     }
 };
