49c722035779ebd5553c7b6ec30bb6f6aa760df6
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-05-19 15:41:21 +0530

    Time: 278 ms (44.15%) | Memory: 121.7 MB (55.84%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,46 @@
+class MedianFinder {
+private:
+    priority_queue<int> lower; //MaxHeap for smaller subtree
+    priority_queue <int, vector<int>, greater<int>> higher; //MinHeap for greater subtree
+public:
+    MedianFinder() {
+
+    }
+    
+    void addNum(int num) {
+        lower.push(num); //push the number into lower subtree
+
+        //incase lower subtree has a element greater than higher subtree elements
+        if(!lower.empty() && !higher.empty() && lower.top() > higher.top())
+        {
+            higher.push(lower.top());
+            lower.pop();
+        }
+
+        if(lower.size() > higher.size() + 1) //Incase lower is greater than higher
+        {
+            higher.push(lower.top());
+            lower.pop();
+        }
+
+        else if(higher.size() > lower.size() + 1) //Incase higher is greater than lower
+        {
+            lower.push(higher.top());
+            higher.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(lower.size() > higher.size()) return (double)lower.top();
+        if(higher.size() > lower.size()) return (double)higher.top();
+
+        return (((double)higher.top())+((double)lower.top()))/2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
