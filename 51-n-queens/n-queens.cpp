f28271fdf172725bfb64f9afe6895996da1032cd
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-11 18:08:42 +0530

    Time: 3 ms (79.28%) | Memory: 9.5 MB (63.32%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    void helper(vector<string>& board, vector<vector<string>>& ans, vector<int>& leftRow,
+                vector<int>& upperDaig, vector<int>& lowerDaig, int n, int col)
+    {
+        if(col == n) //We have fixed all the columns!! Solved
+        {
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row = 0; row < n; row++)
+        {
+            if(leftRow[row]==0 &&               //Left row
+                lowerDaig[row + col] == 0 &&    //Lower Daig check
+                upperDaig[n-1 + col - row] == 0)//Upper Daig check
+            {
+                board[row][col] = 'Q'; //Set the Queen in this row
+                
+                leftRow[row] = 1;             //Set the hashes
+                lowerDaig[row+col] = 1;
+                upperDaig[n-1 +col -row] = 1;
+
+                helper(board, ans, leftRow, upperDaig, lowerDaig, n, col+1);
+
+                //Backtrack
+                board[row][col] = '.';
+
+                leftRow[row] = 0;             //Reset the hashes
+                lowerDaig[row+col] = 0;
+                upperDaig[n-1 +col -row] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans; //Create ans array
+
+        vector<string> board(n); //Create the board
+        string s(n,'.');
+        for(int i=0; i<n; i++) board[i] = s;
+
+        vector<int> leftRow(n,0);         //Check for left daigonal
+        vector<int> upperDaig(2*n+1, 0); //Check for upper daigonal
+        vector<int> lowerDaig(2*n+1,0); //Check for lower daigonal
+
+        helper(board, ans, leftRow, upperDaig, lowerDaig, n, 0);
+
+        return ans;
+    }
+};
