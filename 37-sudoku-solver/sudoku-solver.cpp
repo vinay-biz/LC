c589cd93e047ba9d7c6d784c0fce62f2e65d5b94
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-11 21:22:45 +0530

    Time: 14 ms (44.82%) | Memory: 7.9 MB (19.02%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board);
+    }
+
+    bool helper(vector<vector<char>>& board)
+    {
+        for(int i=0; i<board.size(); i++)
+        {
+            for(int j=0; j<board[0].size(); j++) //Go over the whole board
+            {
+                if(board[i][j] == '.') //if it is a blank
+                {
+                    for(char num='1'; num <='9'; num++) //Try filling up numbers
+                    {
+                        if(isValid(board, i, j, num)) //Check if a number is valid to be filled
+                        {
+                            board[i][j] =  num; //Number to ascii
+
+                            if(helper(board)==true) return true; //Check if board is legal
+
+                            else board[i][j] = '.'; //backtrack
+                        }
+                    }
+
+                    return false; //No number can be filled
+                }
+            }
+        }
+
+        return true; //All numbers were filled
+    }
+
+    bool isValid(vector<vector<char>> &board, int row, int col, char num)
+    {
+        for(int i=0; i<=8; i++)
+        {
+            if(board[i][col] == num) return false; //Check rwo
+            if(board[row][i] == num) return false; //check col
+
+            if(board[3 * (row/3) + i / 3][3 * (col/3) + i % 3] == num) return false;
+        }
+
+        return true;
+    }
+};
