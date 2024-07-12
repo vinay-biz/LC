538f2952f53b457a46fa6d724009e022e0601875
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-12 12:41:50 +0530

    Time: 10 ms (99.23%) | Memory: 8 MB (89.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,20 +5,19 @@ public:
         int n = text1.length();
         int m = text2.length();
         //Initialize DP array
-        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
-
-        for(int i=0; i<n; i++) dp[i][0] = 0; //Base case
-        for(int j=0; j<m; j++) dp[0][j] = 0; //Base case
+        vector<int> prev(m+1,0), curr(m+1,0);
+        for(int j=0; j<m; j++) prev[j] = 0; //Base case
 
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=m; j++)
             {
-                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1]; //Incase common
-                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]); //If not common, take - not take
+                if(text1[i-1] == text2[j-1]) curr[j] = 1 + prev[j-1]; //Incase common
+                else curr[j] = max(curr[j-1], prev[j]); //If not common, take - not take
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
