d460bff01d5441a7ae721fb049fcfed1c80e32b9
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-13 21:58:00 +0530

    Time: 2 ms (81.30%) | Memory: 7.5 MB (12.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0) return 0; //edge case
-        vector<int> dp(n+1,0);
+        //vector<int> dp(n+1,0);
         
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev2 = 0;
+        int prev = 1;
+        int curr;
 
         for(int i=2; i<=n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2]; 
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr; 
         }
 
-        return dp[n];
+        return prev;
     }
 };
