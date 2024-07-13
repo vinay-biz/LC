0c3d8e12f00fd2362193fdc4c842583f69bad4d1
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-13 21:58:30 +0530

    Time: 3 ms (67.33%) | Memory: 7.4 MB (12.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0) return 0; //edge case
-        //vector<int> dp(n+1,0);
-        
+        if(n == 0) return 0; 
+                
         int prev2 = 0;
         int prev = 1;
         int curr;
