dbd86c94693095b6e4a9f3c97847cabf090d5887
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-14 23:16:27 +0530

    Time: 2 ms (42.33%) | Memory: 9.7 MB (6.07%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -4,29 +4,28 @@ public:
         int n = nums.size(); if(n == 0) return 0; if(n == 1) return nums[0];
         vector<int> dp(n,-1); int take,notTake;
 
-        dp[0] = nums[0];
+        int val1, val2, curr, prev2 = 0, prev = nums[0];
         
         for(int i=1; i<n-1; i++)
         {
-            take =  nums[i]; if(i >= 2) take += dp[i-2];
-            notTake = 0 + dp[i-1];
+            take =  nums[i]; if(i >= 2) take += prev2;
+            notTake = 0 + prev;
+            curr = max(take, notTake);
 
-            dp[i] = max(take, notTake);
-        }
-        int val1 = dp[n-2];
+            prev2 = prev; prev = curr;
+        } val1 = prev;
 
-        dp = vector<int> (n,-1);
-    
-        dp[1] = nums[1];
+        prev2 = 0; prev = nums[1];
 
         for(int i=2; i<n; i++)
         {
-            take = nums[i]; if(i >= 3) take += dp[i-2];
-            notTake = 0 + dp[i-1];
+            take = nums[i]; if(i >= 3) take += prev2;
+            notTake = 0 + prev;
 
-            dp[i] = max(take, notTake);
-        }
-        int val2 = dp[n-1];
+            curr = max(take, notTake);
+
+            prev2 = prev; prev = curr;
+        }val2 = prev;
         
         return max(val1,val2);
     }
