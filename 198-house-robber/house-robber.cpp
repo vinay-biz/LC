8eab33d7f964d4a5a448cdd68e91c956f1f5e010
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-14 19:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (5.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int n, vector<int> &dp)
-    {
-        if(n < 0) return 0;
-
-        if(dp[n] != -1) return dp[n];
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0]; //edge case
+        vector<int> dp(n,0);
+        
+        dp[0] = nums[0];
+        dp[1] = nums[1];
 
         int left, right;
-        left = helper(nums, n-2,dp) + nums[n]; 
-        right = helper(nums, n-3, dp) + nums[n];
+        for(int i=2; i<n; i++)
+        {   
+            left = (i >= 2) ? dp[i-2] : 0;
+            right = (i >= 3) ? dp[i-3] : 0;
 
-        return dp[n] = max(left,right);
-    }
-    int rob(vector<int>& nums) {
-        int n = nums.size()-1;
-        vector<int> dp(n+1,-1);
+            dp[i] = max(left, right) + nums[i];
+        }
 
-        int left = helper(nums,n,dp);
-        int right = helper(nums, n-1,dp);
-
-        return max(left,right);
+        return max(dp[n-1], dp[n-2]);
     }
 };
