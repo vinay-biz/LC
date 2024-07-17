2fb26001af0cc7798884df47ee7c17ffc7cc71c6
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-17 15:26:42 +0530

    Time: 3 ms (98.92%) | Memory: 17 MB (8.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int col0 = 1;
 
         //Set the boundary
