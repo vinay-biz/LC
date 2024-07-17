80af869b5df1029c332dfd4680d5651cef5f203d
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-17 15:26:16 +0530

    Time: 12 ms (39.77%) | Memory: 16.8 MB (15.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>& matrix, int row, int col)
-    {
-        for(int i=0; i<matrix.size(); i++) matrix[i][col] = 0;
-        for(int j=0; j<matrix[0].size(); j++) matrix[row][j] = 0;
-    }
-    
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> zeros;
+        int col0 = 1;
 
-        for(int i=0; i<matrix.size(); i++)
-        {
-            for(int j=0; j<matrix[0].size(); j++)
-            {
-                if(matrix[i][j] == 0) zeros.push_back({i,j});
+        //Set the boundary
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    
+                    matrix[i][0] = 0; //Boundary element marked
+                    if(j != 0) matrix[0][j] = 0; //Boundary element marked
+                    else col0 = 0;
+                }
             }
         }
 
-        for(auto x : zeros)
-        {
-            helper(matrix, x.first, x.second);
+        //Set 0th row and column
+        for(int i=1; i<matrix.size(); i++){
+            for(int j=1; j<matrix[0].size(); j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                    matrix[i][j] = 0;
+                
+            }
         }
+
+        if(matrix[0][0] == 0) for(int i=0; i<matrix[0].size(); i++) matrix[0][i] = 0;
+        if(col0 == 0) for(int i=0; i<matrix.size(); i++) matrix[i][0] = 0;
+
     }
 };
