77acfd413dda2053ca092630477b7e0fdd16181d
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-18 13:19:59 +0530

    Time: 330 ms (18.26%) | Memory: 16.5 MB (61.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size(); int sum = 0;
-        if(n == 1) return false; 
+        //if(n == 1) return false; 
         //If the array sum is odd, it cannot be divied into two equals
         for(int i=0; i<n; i++) sum += nums[i]; if(sum%2 == 1) return false; else sum = sum/2;
         vector<vector<bool>> dp(n, vector<bool> (sum+1, false));
