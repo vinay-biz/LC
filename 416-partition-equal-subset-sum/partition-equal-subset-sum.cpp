3a72838edfcb3bdd3f46b5b727592bfcc6d83325
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-18 13:27:30 +0530

    Time: 271 ms (31.08%) | Memory: 13 MB (88.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,13 +4,12 @@ public:
         int n = nums.size(); int sum = 0;
         
         for(int i=0; i<n; i++) sum += nums[i]; if(sum%2 == 1) return false; else sum = sum/2;
-        //vector<vector<bool>> dp(n, vector<bool> (sum+1, false));
         vector<bool> prev(sum+1, false), curr(sum+1, false);
-        prev[0] = curr[0] =  true; //Base case 1
+        prev[0]  =  true; //Base case 1
         if(nums[0] <= sum) prev[nums[0]] = true; //Base case 2
 
         for(int i = 1; i < n; i++) {
-            for(int j = 1; j <= sum; j++) {
+            for(int j = 0; j <= sum; j++) {
                 bool notTake = prev[j];
                 bool take = false;
                 if(nums[i] <= j) take = prev[j-nums[i]];
