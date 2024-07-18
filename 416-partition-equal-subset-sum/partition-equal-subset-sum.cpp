535e5f422a28e2b0a1265991dc5e955d9bf46a5a
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-18 13:23:40 +0530

    Time: 270 ms (31.57%) | Memory: 12.9 MB (88.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size(); int sum = 0;
-        //if(n == 1) return false; 
-        //If the array sum is odd, it cannot be divied into two equals
+        
         for(int i=0; i<n; i++) sum += nums[i]; if(sum%2 == 1) return false; else sum = sum/2;
-        vector<vector<bool>> dp(n, vector<bool> (sum+1, false));
-
-        for(int i=0; i<n; i++) dp[i][0] = true; //Base case 1
-        if(nums[0] <= sum) dp[0][nums[0]] = true;
+        //vector<vector<bool>> dp(n, vector<bool> (sum+1, false));
+        vector<bool> prev(sum+1, false), curr(sum+1, false);
+        prev[0] = curr[0] =  true; //Base case 1
+        if(nums[0] <= sum) prev[nums[0]] = true; //Base case 2
 
         for(int i = 1; i < n; i++) {
             for(int j = 1; j <= sum; j++) {
-                bool notTake = dp[i-1][j];
+                bool notTake = prev[j];
                 bool take = false;
-                if(nums[i] <= j) take = dp[i-1][j-nums[i]];
+                if(nums[i] <= j) take = prev[j-nums[i]];
 
-                dp[i][j] = (take ||notTake);
+                curr[j] = (take ||notTake);
             }
+            prev = curr;
         }
 
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
