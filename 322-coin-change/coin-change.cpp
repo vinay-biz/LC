3b7db0de1c9f26f15b7d96c8e818e3ab4c224997
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-20 15:16:21 +0530

    Time: 42 ms (44.34%) | Memory: 47.2 MB (28.70%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int>& coins, int n, int amount, vector<vector<int>>& dp)
-    {
-        if(amount == 0) return 0;
-        if(n == 0)
-        {
-            if(amount%coins[0] == 0) return amount/coins[0];
-            return 1e9; 
-        }
-
-        if(dp[n][amount] != -1) return dp[n][amount];
-
-        int notTake = 0 + helper(coins, n-1, amount, dp);
-        int take = 1e9;
-        if(coins[n] <= amount) take = 1+helper(coins, n, amount - coins[n], dp);
-
-        return dp[n][amount] = min(take, notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+        vector<vector<int>> dp(n, vector<int> (amount+1, 1e9));
 
-        //for(int i=0; i<)
+        for(int i=0; i<n; i++) dp[i][0] = 0;
 
+        for(int i =coins[0]; i <= amount; i++) {
+            if(i % coins[0] == 0) dp[0][i] = i/coins[0];
+            else dp[0][i] == 1e9;  
+        }
 
-        int ans =  helper(coins, n-1, amount, dp);
-        
-        if(ans == 1e9) return -1;
-        return ans;
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<=amount; j++)
+            {
+                int notTake = 0 + dp[i-1][j];
+                int take = 1e9;
+                if(coins[i] <= j) take = 1 + dp[i][j - coins[i]];
+
+                dp[i][j] = min(take, notTake);
+            }
+        }
+
+        if(dp[n-1][amount] == 1e9) return -1;
+        return dp[n-1][amount];
     }
 };
