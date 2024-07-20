ae554896957fdb73e97c6e2229d8b1a2126fb4c9
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-20 13:44:42 +0530

    Time: 17 ms (82.87%) | Memory: 20.8 MB (18.30%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int n = g.size(), m = s.size(), i = 0, j = 0, ans = 0;
 
         sort(g.begin(), g.end());
