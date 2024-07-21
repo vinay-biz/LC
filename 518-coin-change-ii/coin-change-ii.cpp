3a663fa102d35ae80a90d3c4dcabac6578f1ec6d
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-22 01:22:11 +0530

    Time: 11 ms (54.78%) | Memory: 20 MB (36.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int helper(vector<int>& coins, int t, int n, vector<vector<int>>& dp)
-    {
-        if(t == 0) return 1; //We reached the sum
-        if(n == 0) //If it is the last coin
+    
+    int change(int t, vector<int>& coins) {
+        int n = coins.size(); vector<vector<int>> dp(n, vector<int> (t+1, 0));
+
+        //for(int i=0; i<n; i++) dp[i][0] = 1;
+        for(int i=0; i<=t; i++) 
         {
-            if(t >= coins[0] && t%coins[0] == 0) return 1; //If current coins can satisfy
-            return 0;
+            dp[0][i] = (i%coins[0] == 0);
         }
 
-        if(dp[n][t] != -1) return dp[n][t];
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<=t; j++)
+            {
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(coins[i] <= j) take = dp[i][j - coins[i]];
 
-        int notTake = helper(coins, t, n-1, dp);
-        int take = 0;
-        if(coins[n] <= t) take = helper(coins, t - coins[n], n, dp);
+                dp[i][j] = take + notTake;
+            }
+        }
 
-        return dp[n][t] = take+notTake;
-    }
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        return helper(coins, amount, n-1, dp);
+        return dp[n-1][t];
     }
 };
