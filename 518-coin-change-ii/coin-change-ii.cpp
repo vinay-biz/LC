a4391d17547304fcdb50f129ac066c539a97b2a1
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-22 01:33:48 +0530

    Time: 13 ms (43.15%) | Memory: 9 MB (83.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int change(int t, vector<int>& coins) {
-        int n = coins.size(); vector<vector<int>> dp(n, vector<int> (t+1, 0));
-        vector<int> prev(t+1,0), curr(t+1,0);
-        for(int i=0; i<=t; i++) prev[i] = (i%coins[0] == 0);
+        vector<int> prev(t+1,0), curr(t+1,0); for(int i=0; i<=t; i++) prev[i] = (i%coins[0] == 0);
 
-        for(int i=1; i<n; i++){
+        for(int i=1; i<coins.size(); i++){
             for(int j=0; j<=t; j++){
                 int notTake = prev[j];
                 int take = 0;
