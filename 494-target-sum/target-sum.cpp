0e36402bf7f9bb3c26fd6989b9a9caab3934299f
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-21 23:50:35 +0530

    Time: 4 ms (84.03%) | Memory: 11.4 MB (65.65%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totSum = 0,n = nums.size(); 
-        
-        for(int i=0; i<n; i++) 
-        totSum += nums[i];
-        
-        int val = totSum - target;
-        
-        if((val)%2 != 0 || val < 0) return 0; 
-        int sum = val/2; 
-        
-        vector<vector<int>> dp(n, vector<int> (sum+1, 0));
-        if(nums[0] == 0) dp[0][0] = 2; else dp[0][0] = 1;
-        //for(int i=0; i<n; i++) dp[i][0] = 1;
-        if( nums[0] != 0 && nums[0] <= sum) dp[0][nums[0]] = 1;
+        int totSum = 0,n = nums.size(); for(int i=0; i<n; i++) totSum += nums[i];
+        int val = totSum - target; if((val)%2 != 0 || val < 0) return 0; int sum = val/2; 
+        //vector<vector<int>> dp(n, vector<int> (sum+1, 0));
+        vector<int> prev(sum+1, 0), curr(sum+1, 0);
 
-        for(int i=1; i<n; i++)
-        {
-            for(int j=0; j<=sum; j++)
-            {
-                int notTake = dp[i-1][j];
+        if(nums[0] == 0) prev[0] = 2; else prev[0] = 1;
+        if( nums[0] != 0 && nums[0] <= sum) prev[nums[0]] = 1;
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=sum; j++){
+                int notTake = prev[j];
                 int take = 0;
-                if(nums[i] <= j) take = dp[i-1][j-nums[i]];
+                if(nums[i] <= j) take = prev[j-nums[i]];
 
-                dp[i][j] = take + notTake;
+                curr[j] = take + notTake;
             }
+            prev = curr;
         }
-
-
-
-
-        return dp[n-1][sum];
+        return prev[sum];
     }
     /*
         s1 - s2 = target
