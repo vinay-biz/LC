a09eb7a4cc2ccab07663c3b219dd7a3b4c3e6dd4
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-21 23:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (48.35%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int n, int sum, vector<vector<int>>& dp)
-    {
-        if(n == 0)
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totSum = 0,n = nums.size(); 
+        
+        for(int i=0; i<n; i++) 
+        totSum += nums[i];
+        
+        int val = totSum - target;
+        
+        if((val)%2 != 0 || val < 0) return 0; 
+        int sum = val/2; 
+        
+        vector<vector<int>> dp(n, vector<int> (sum+1, 0));
+        if(nums[0] == 0) dp[0][0] = 2; else dp[0][0] = 1;
+        //for(int i=0; i<n; i++) dp[i][0] = 1;
+        if( nums[0] != 0 && nums[0] <= sum) dp[0][nums[0]] = 1;
+
+        for(int i=1; i<n; i++)
         {
-            if(sum == 0 && nums[0] == 0) return 2;
-            if(sum == 0 ||sum == nums[0]) return 1;
-            return 0;
+            for(int j=0; j<=sum; j++)
+            {
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(nums[i] <= j) take = dp[i-1][j-nums[i]];
+
+                dp[i][j] = take + notTake;
+            }
         }
 
-        if(dp[n][sum] != -1) return dp[n][sum];
 
-        int notTake = helper(nums, n-1, sum, dp);
-        int take = 0;
-        if(nums[n] <= sum) take = helper(nums, n-1, sum-nums[n], dp);
 
-        return dp[n][sum] = take + notTake; 
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int totSum = 0; for(int i=0; i<n; i++) totSum += nums[i];
 
-        int sum;
-        if((totSum - target)%2 != 0) return 0;
-
-        if(totSum-target >= 0) sum = (totSum - target)/2;
-        else return 0;
-
-        vector<vector<int>> dp(n, vector<int> (sum+1, -1));
-        return helper(nums, n-1, sum, dp);
+        return dp[n-1][sum];
     }
     /*
         s1 - s2 = target
