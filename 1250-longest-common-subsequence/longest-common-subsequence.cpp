efed8f0233fc3c338c9c6c6b6cedc12e28e410c9
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-22 21:56:46 +0530

    Time: 31 ms (38.21%) | Memory: 26.1 MB (7.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    
+    int helper(string& str1, string& str2, int n, int m, vector<vector<int>>& dp)
+    {
+        if(n < 0 || m < 0) return 0;
+
+        if(dp[n][m] != -1) return dp[n][m];
+
+        if(str1[n] == str2[m]) return 1 + helper(str1, str2, n-1, m-1, dp);
+
+        return dp[n][m]= max( helper(str1, str2, n-1, m, dp), helper(str1, str2, n, m-1, dp));
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        std::ios_base::sync_with_stdio(false);std::cout.tie(nullptr);std::cin.tie(nullptr);
-        int n = text1.length();
-        int m = text2.length();
-        //Initialize DP array
-        vector<int> prev(m+1,0), curr(m+1,0);
-        for(int j=0; j<m; j++) prev[j] = 0; //Base case
-
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=1; j<=m; j++)
-            {
-                if(text1[i-1] == text2[j-1]) curr[j] = 1 + prev[j-1]; //Incase common
-                else curr[j] = max(curr[j-1], prev[j]); //If not common, take - not take
-            }
-            prev = curr;
-        }
-
-        return prev[m];
+        int n = text1.length(), m = text2.length();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return helper(text1, text2, n-1, m-1, dp);
     }
 };
