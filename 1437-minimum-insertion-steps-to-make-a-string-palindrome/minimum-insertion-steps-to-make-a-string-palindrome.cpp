e6981cbe75819d5668311e5e5c0878535267f101
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-23 14:07:15 +0530

    Time: 25 ms (72.88%) | Memory: 8.3 MB (85.82%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -17,6 +17,19 @@ public:
         return prev[n];
     }
     int minInsertions(string s) {
-        return s.size()-LPS(s);
+        int n = s.size();
+        string t = s; reverse(t.begin(), t.end());
+
+        vector<int> prev(n+1, 0), curr(n+1,0);
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s[i-1] == t[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j],curr[j-1]);
+            }
+            prev = curr;
+        }
+
+        return n-prev[n];
     }
 };
