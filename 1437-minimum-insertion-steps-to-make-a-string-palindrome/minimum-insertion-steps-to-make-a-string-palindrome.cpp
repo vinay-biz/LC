770a989527942cc98d2a2c788f1decd60aae3ab5
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-23 14:07:33 +0530

    Time: 25 ms (72.88%) | Memory: 8.4 MB (85.82%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
-    int LPS(string s) {
-        string t = s; reverse(t.begin(), t.end());
-        int n = s.length();
-
-        vector<int> prev(n+1,0), curr(n+1,0);
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(s[i-1] == t[j-1]) curr[j] = 1 + prev[j-1];
-                else curr[j] = max(prev[j], curr[j-1]);
-            }
-            prev = curr;
-        }
-
-        return prev[n];
-    }
     int minInsertions(string s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int n = s.size();
         string t = s; reverse(t.begin(), t.end());
 
