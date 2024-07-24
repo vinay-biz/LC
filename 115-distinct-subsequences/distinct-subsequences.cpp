b2c21eaafa6f0ddae314bd2dacad8de0ac88a53a
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-24 14:20:59 +0530

    Time: 17 ms (65.46%) | Memory: 19.9 MB (58.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int helper(string& s, string& t, int n, int m, vector<vector<int>>& dp)
-    {
-        if(m < 0) return 1;
-        if(n < 0) return 0;
-        
-        if(dp[n][m] != -1) return dp[n][m];
-
-        if(s[n] == t[m])
-        {
-            int take = helper(s, t, n-1, m-1, dp);
-            int notTake = helper(s, t, n-1, m, dp);
-
-            return dp[n][m] = take+notTake;
-        }
-
-        return dp[n][m] =  helper(s, t, n-1, m, dp);
-    }
     int numDistinct(string s, string t) {
         int n = s.length(), m = t.length();
-        vector<vector<int>> dp(n, vector<int> (m,-1));
-        return helper(s, t, n-1, m-1,dp);
+        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
+        
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++)
+            {
+                if(s[i-1] == t[j-1]) {
+                    if(dp[i-1][j-1] <= INT_MAX - dp[i-1][j]) 
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        } 
+
+        return (int)dp[n][m];
     }
 };
