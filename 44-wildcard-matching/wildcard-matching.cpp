90c376f53ff0b2f4e578acb5af53af3a139cd560
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-24 17:57:02 +0530

    Time: 3 ms (98.49%) | Memory: 10 MB (94.01%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.size(),  m = p.size(); 
-        vector<bool> prev(m+1, false), curr(m+1, false);
-
-        prev[0] = true;
-
-        for(int j=1; j<=m; j++) {
-            bool flag = true;
-            for(int i=1; i<=j; i++){
-                if(p[i-1] != '*') {
-                    flag = false; break;
-                }
+        int m = s.length(), n = p.length();
+        int i = 0, j = 0, asterick = -1, match;
+        while (i < m) {
+            if (j < n && p[j] == '*') {
+                match = i;  
+                asterick = j++;
             }
-            prev[j] = flag;
-        }
-
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=1; j<=m; j++)
-            {
-                if(s[i-1] == p[j-1] || p[j-1] == '?') curr[j] = prev[j-1];
-                else if(p[j-1] == '*') curr[j] = (prev[j] || curr[j-1]);
-                else curr[j] = false;
+            else if (j < n && (s[i] == p[j] || p[j] == '?')) {
+                i++;
+                j++;
             }
-            prev = curr;
+            else if (asterick >= 0) {
+                i = ++match;
+                j = asterick + 1;
+            }
+            else return false;
         }
-
-        return prev[m];
+        while (j < n && p[j] == '*') j++;
+        return j == n;
     }
 };
