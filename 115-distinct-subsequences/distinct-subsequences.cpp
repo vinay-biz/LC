bb13388b5242a88b1e7e2a0d0f0b61c8c9b45862
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-24 14:24:57 +0530

    Time: 15 ms (74.73%) | Memory: 20.1 MB (53.49%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -8,7 +8,7 @@ public:
         prev[0] = 1; curr[0] = 1;
 
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++)
+            for(int j=m; j>=1; j--)
             {
                 if(s[i-1] == t[j-1]) {
                     if(prev[j-1] <= INT_MAX - prev[j]) 
