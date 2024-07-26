40d3ff51b7beac30b0e68472dcb65ebbfe21f55c
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-26 13:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (78.86%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& price) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int n = k*2;
         vector<int> ahead(n+1, 0), curr(n+1, 0);
 
