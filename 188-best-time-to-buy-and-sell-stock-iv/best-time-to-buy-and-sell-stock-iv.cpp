2867ce9148a8bd0d8e0d8ece277e1d9b71a48bcd
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-26 13:32:24 +0530

    Time: 6 ms (79.26%) | Memory: 16.3 MB (54.96%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
-    int helper(vector<int>& price, int k, int n, int curr, vector<vector<int>>& dp)
-    {
-        if(n == price.size() || curr == k * 2) return 0;
-
-        if(dp[n][curr] != -1) return dp[n][curr];
-
-        int buy = 0, sell = 0;
-        if(curr % 2 == 0) buy = helper(price, k, n+1, curr+1, dp) - price[n];
-        if(curr % 2 == 1) sell = helper(price, k, n+1, curr+1, dp) + price[n];
-        int none = helper(price, k, n+1, curr, dp);
-
-        return dp[n][curr] = max(max(buy, sell), none);
-    }
     int maxProfit(int k, vector<int>& price) {
         int n = price.size(); vector<vector<int>> dp(n+1, vector<int> ((k*2)+1, 0));
 
-        for(int i=n-1; i >= 0; i--) {
-            for(int j=(k*2)-1; j >= 0; j--)
+        for(int i = n-1; i >= 0; i--) {
+            for(int j= (k*2)-1 ; j >= 0; j--)
             {
                 int buy=0, sell = 0;
                 if(j % 2 == 0) buy = dp[i+1][j+1] - price[i];
