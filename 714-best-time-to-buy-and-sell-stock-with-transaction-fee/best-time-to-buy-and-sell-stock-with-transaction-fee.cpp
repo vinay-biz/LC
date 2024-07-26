e51fd41b04eb22af0b6aa737e809b29a545eeaf1
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-26 19:19:01 +0530

    Time: 23 ms (99.98%) | Memory: 57.2 MB (96.26%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int curr0=0, curr1=0, ahead0=0, ahead1=0;
         
-        for(int i = n-1; i >= 0; i--) {
+        for(int i = prices.size()-1; i >= 0; i--) {
             curr0 = max(ahead1 - prices[i] - fee, ahead0);
             curr1 = max(ahead0 + prices[i],ahead1);
 
