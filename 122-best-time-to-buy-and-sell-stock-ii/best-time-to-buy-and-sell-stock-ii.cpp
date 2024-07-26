61db2526c3ad3f1d001510fa2e981b010bcd0545
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-26 12:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (39.43%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& price) {
+        int ans = 0;
+        for(int i=1; i<price.size(); i++)
+        {
+            if(price[i] > price[i-1]) ans+= price[i] - price[i-1];
+        }
+        return ans;
+    }
+};
