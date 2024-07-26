b3a8e8e63e731e6ebbb4fa95c40770f888975270
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-26 14:26:15 +0530

    Time: 9 ms (16.97%) | Memory: 14.7 MB (17.57%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    int helper(vector<int>& price, int n, int curr, vector<vector<int>>& dp)
-    {
-        if(n >= price.size()) return 0;
-
-        if(dp[n][curr] != -1) return dp[n][curr];
-
-        int buy = 0, sell = 0;
-        if(!curr) buy = helper(price, n+1, !curr, dp) - price[n];
-        if(curr) sell = helper(price, n+2, !curr, dp) + price[n];
-        int none = helper(price, n+1, curr, dp);
-
-        return dp[n][curr] = max(max(buy,sell),none);
-    }
     int maxProfit(vector<int>& price) {
         int n = price.size(); vector<vector<int>> dp(n+2, vector<int> (2, 0));
-    
+        vector<int> curr(2, 0), ahead1(2, 0), ahead2(2,0);
 
         for(int i = n-1; i >= 0; i--)
         {
-            for(int j = 1; j >= 0; j--)
-            {
-                int buy = 0, sell = 0;
-                if(!j) buy = dp[i+1][!j] - price[i];
-                if(j)  sell = dp[i+2][!j] + price[i];
-                int none = dp[i+1][j];
+            curr[0] = max(ahead1[1] - price[i], ahead1[0]);
+            curr[1] = max(ahead2[0] + price[i], ahead1[1]);
 
-                dp[i][j] = max(max(buy,sell),none);
-            }
+            ahead2 = ahead1;
+            ahead1 = curr;
         }
-        return dp[0][0];
+        return ahead1[0];
     }
 };
