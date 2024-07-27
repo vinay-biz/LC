5787e556ef5a2dc79af6af39c5552d97c0199755
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-27 14:03:11 +0530

    Time: 81 ms (59.35%) | Memory: 13.3 MB (46.50%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int n, int prev, vector<vector<int>>& dp)
-    {
-        if(n == nums.size()) return 0;
-
-        if(dp[n][prev+1] != -1) return dp[n][prev+1];
-
-        int skip = 0 + helper(nums, n+1, prev, dp);
-        if(prev == -1 || nums[n] > nums[prev]) skip = max (skip, 1 + helper(nums, n+1, n, dp));
-
-        return dp[n][prev+1] = skip;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        int n = nums.size(); vector<int> dp(n, 1);
+        int ans = 1;
 
-        for(int i= n-1; i>=0; i--)
+        for(int i=1; i<n; i++)
         {
-            for(int j = i-1; j >= -1; j--)
+            for(int j = i-1; j >= 0; j--)
             {
-                int skip = 0 + dp[i+1][j+1];
-                if(j == -1 || nums[i] > nums[j])
-                    skip = max(skip, 1 + dp[i+1][i+1]);
-
-                dp[i][j+1] = skip;
+                if(nums[j] < nums[i] && dp[i] < dp[j] + 1)
+                {
+                    dp[i] = dp[j]+1;
+                    ans = max(ans, dp[i]);
+                }
             }
         }
-        return dp[0][0];
+
+        return ans;
     }
 };
