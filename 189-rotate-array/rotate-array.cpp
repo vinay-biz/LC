8c526ed1c4c096e6aeb79ca892f5b661d8ffd47b
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-31 23:26:49 +0530

    Time: 8 ms (99.52%) | Memory: 27.4 MB (56.61%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size(); k = k%n;
-
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin()+k);
-        reverse(nums.begin()+k, nums.end());
+         ios_base::sync_with_stdio(false);
+      k = k % nums.size(); // if the array size is 7 and k=7 it means rotating the array by k will result in same array , so array will only be rotated when k is not a multiple of the size of array , it reduces the number of operations needs to be performed.in this method the time complexity is O(n) , and space is O(1)
+      reverse(nums.begin() , nums.begin()+nums.size()-k);
+      reverse(nums.begin()+(nums.size()-k) , nums.end());
+      reverse(nums.begin(), nums.end());
     }
 };
