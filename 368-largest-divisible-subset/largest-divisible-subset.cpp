9534291326132e862a9f8a119f99150dd9514ad2
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-07-31 21:39:24 +0530

    Time: 16 ms (49.04%) | Memory: 11.7 MB (15.96%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int n = nums.size();
-        vector<int> dp(nums.size(), 1);
-        vector<int> hash(nums.size());
+        vector<int> dp(n,1);
+        vector<int> hash(n,1);
+
+        sort(nums.begin(), nums.end());
 
         int maxi = 1;
         int lastIndex = 0;
 
-        sort(nums.begin(), nums.end());
-
-        for(int i=0; i<nums.size(); i++)
+        for(int i=0; i<n; i++)
         {
             hash[i] = i;
-            for(int j = 0; j < i; j++)
+            for(int j=0; j<i; j++)
             {
-                if(nums[i] % nums[j] == 0 && dp[j]+1 > dp[i] )
+                if((nums[i] % nums[j] == 0) && dp[j]+1 > dp[i])
                 {
                     dp[i] = dp[j] + 1;
                     hash[i] = j;
                 }
             }
+
             if(dp[i] > maxi)
             {
                 maxi = dp[i];
@@ -28,16 +32,17 @@ public:
             }
         }
 
-        vector<int> temp;
-
+        vector<int> ans;
         int index = lastIndex;
-        while( index != hash[index])
+
+        while(hash[index] != index)
         {
-            temp.push_back(nums[index]);
+            ans.push_back(nums[index]);
             index = hash[index];
         }
-        temp.push_back(nums[index]);
-        reverse(temp.begin(), temp.end());
-        return temp;
-    }
+
+        ans.push_back(nums[index]);
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }   
 };
