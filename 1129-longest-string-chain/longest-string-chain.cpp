b50785a047f53cbc2cb75b48162ac39be84fc781
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-01 11:02:10 +0530

    Time: 48 ms (90.93%) | Memory: 17.2 MB (59.19%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool cmp(string& next, string& prev)
+    {
+        if(next.size() !=  1+prev.size()) return false;
+
+        int nextPtr = 0;
+        int prevPtr = 0;
+
+        int count = 0;
+
+        while(nextPtr < next.size())
+        {
+            if(next[nextPtr] != prev[prevPtr]) nextPtr++;
+            
+            else
+            {
+                nextPtr++;
+                prevPtr++;
+            }
+        }
+
+        if(nextPtr == next.size() && prevPtr == prev.size()) return true;
+        return false;
+    }
+    static bool comp(string &s1, string &s2)
+    {
+        return s1.size() < s2.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        vector<int> dp(n,1);
+        int ans = 1;
+
+        sort(words.begin(), words.end(), comp);
+
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(cmp(words[i], words[j]) && dp[j]+1 > dp[i])
+                {
+                    dp[i] = dp[j]+1;
+                }
+            }
+            if(dp[i] > ans) ans = dp[i];
+        }
+
+        return ans;
+    }
+};
