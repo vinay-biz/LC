fe3f2d18991066a9bad4adb0cda2dc9d9977a004
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-02 13:09:24 +0530

    Time: 4 ms (80.21%) | Memory: 8.7 MB (54.12%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,46 +1,53 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int left=0, right=0; //set left and right pointer
-        unordered_map <char,int> mp;
+        int n = s1.length();
+        int m = s2.length();
+        if(n > m) return false;
+        
+        vector<int> mp1(26, 0);
+        vector<int> mp2(26, 0);
 
-        for(int i=0; i<s1.size(); i++) mp[s1[i]]++;
-
-        while(left < s2.size() && right < s2.size())
+        //Initialise the maps;
+        for(int i=0; i<s1.length(); i++) 
         {
-            if(mp.find(s2[right])==mp.end()) //incase the element is not a part
-            {
-                while(left < right) //make the hashmap right again
-                {
-                    mp[s2[left]]++;
-                    left++;
-                }
-                left = right + 1;
-                right = right + 1;
-            }
-
-            else //incase element is a part 
-            {
-                if(mp[s2[right]] > 0) //if we haven't gone to minus value yet
-                {
-                    mp[s2[right]]--;
-                    if((right-left+1) == s1.length()) return true; //maybe we're just done
-                    right++; //otherwise next element please
-                }
-
-                if(mp[s2[right]] == 0) //incase the values are gonna go to negitive
-                {
-                    while(mp[s2[right]] == 0) //lets reduce left till it can be poititve again
-                    {
-                        mp[s2[left]]++;
-                        left++;
-                    } //remember no shifting right yet, we have to still check the new string
-                }
-            }
+            mp1[s1[i] - 'a']++;
+            mp2[s2[i] - 'a']++;
         }
 
-        return false; //No luck :(
+        //Matintain match count
+        int match = 0;
+        for(int i = 0; i<26; i++)
+        {
+            if(mp1[i] == mp2[i] ) match++;
+        }
 
+        int left = 0;
+        //Sliding Window
+        for(int right = n; right < m; right++)
+        {
+            if(match == 26) return true;
 
+            int index = s2[right] - 'a'; //Which is the next character to include
+
+            //Include the next character
+            mp2[index]++;
+            if(mp1[index] == mp2[index]) match++; //If by increasing the count becomes equal
+            else if(mp1[index]+1 == mp2[index]) //If including the charcter exceed count
+            match--;
+
+            //Remove the left charcter
+
+            index = s2[left] - 'a'; //Which is the character to remove
+
+            //Remove the last character
+            mp2[index]--;
+            if(mp1[index] == mp2[index]) match++; //If by decreasing the count becomes equal
+            else if(mp1[index]-1 == mp2[index]) //If removing, the charcter dimineshes count
+            match--;
+            left++;
+        }
+
+        return (match == 26);
     }
 };
