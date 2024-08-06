4dd3b517c3d18b648d3534b5a9f8594e289fd1df
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-07 01:57:03 +0530

    Time: 15 ms (70.16%) | Memory: 16.8 MB (78.03%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,53 +1,53 @@
-#define INF 1e9
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        std::ios_base::sync_with_stdio(false);
-        std::cout.tie(nullptr);
-        std::cin.tie(nullptr);
-        vector<vector<vector<int>>> graph(n); //create a adj list out of the edges
-
-        for(int i=0; i < flights.size(); i++)
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        
+        vector<pair<int,int>>adj[n];
+        for(auto& it : flights)
         {
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int w = flights[i][2];
-
-            graph[u].push_back({v,w});
+            adj[it[0]].push_back({it[1], it[2]});
         }
-
-        vector<int> dist(n,INF); //distance array
-        queue <vector<int>> pq; //wt, node, k    
-
-        pq.push({0 ,src, 0});
-        dist[src] = 0; //distance of src from itself is 0
-
-        while(!pq.empty())
+        
+        vector<int>dist(n, 1e9);
+        
+        // {stops, {node, dist}}
+        queue<pair<int,pair<int,int>>>q;
+        
+        q.push({0,{src,0}});
+        
+        dist[src] = 0;
+        
+        while(!q.empty())
         {
-            vector<int> it = pq.front();
-
-            int currDist = it[0];
-            int node = it[1];
-            int kVal = it[2];
-             
-            pq.pop();
-
-            if(kVal > k) break;
-
-            for(auto adj : graph[node])
+            auto p = q.front();
+            q.pop();
+            
+            int stops = p.first;
+            int node = p.second.first;
+            int dis = p.second.second;
+            
+            if(stops > K)
+                break;
+            
+            
+            for(auto& it : adj[node])
             {
-                int adjNode = adj[0];
-                int adjDist = adj[1];
-
-                if((currDist + adjDist) < dist[adjNode]) //incase we found a better route
+                int adjNode = it.first;
+                int cost = it.second;
+                
+                if(dis + cost < dist[adjNode] && stops <= K)
                 {
-                    dist[adjNode] = currDist + adjDist;
-                    pq.push({dist[adjNode], adjNode, kVal + 1});
+                    dist[adjNode] = dis + cost;
+                    q.push({stops + 1, {adjNode, dist[adjNode]}});
                 }
+            
             }
         }
-
-        if(dist[dst] == INF) return -1;
+        
+        
+        if(dist[dst] == 1e9)
+            return -1;
+        
         return dist[dst];
-        }
+    }
 };
