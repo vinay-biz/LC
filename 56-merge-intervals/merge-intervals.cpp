a02486a5a5ca49744b83cf761db1b534b2a778c0
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-08 02:32:35 +0530

    Time: 20 ms (76.88%) | Memory: 23.1 MB (29.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         
