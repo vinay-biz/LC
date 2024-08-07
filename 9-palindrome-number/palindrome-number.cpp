7619600bcd0a42778b3d642ee4dc54192c8d4e01
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-07 19:35:19 +0530

    Time: 16 ms (15.70%) | Memory: 8.3 MB (70.37%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        std::string strX = std::to_string(x);
-        return strX == std::string(strX.rbegin(), strX.rend());
+        if (x < 0) {
+            return false;
+        }
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
     }
 };
