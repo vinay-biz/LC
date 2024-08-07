c71216b411a3de95879640c0a368a88637545497
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-07 20:25:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (27.06%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -12,17 +12,27 @@ public:
 
     int tribonacci(int n)
     {
+        if(n == 0 || n==1) return n;
         vector<int> dp(40,0);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int prev2 = 0;
+        int prev1 = 1;
+        int prev = 1;
+        int curr;
+
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 1;
 
         for(int i=3; i<=n; i++)
         {
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+            curr = prev+prev1+prev2;
+
+            prev2 = prev1;
+            prev1 = prev;
+            prev = curr;
         }
 
-        return dp[n];
+        return prev;
     }
 
 };
