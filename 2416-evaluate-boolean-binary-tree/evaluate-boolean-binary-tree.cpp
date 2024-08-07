d2af9c459f013fc06e6f169f700e62eab42cc4f1
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-07 22:05:28 +0530

    Time: 12 ms (49.41%) | Memory: 17.5 MB (52.76%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -17,10 +17,10 @@ public:
         if(root->val == 0)
             return false;
         if(root->val == 2)
-            return evaluateTree(root->left) or evaluateTree(root->right);
+            return evaluateTree(root->left) || evaluateTree(root->right);
         
         if(root->val == 3)
-            return evaluateTree(root->left) and evaluateTree (root->right);
+            return evaluateTree(root->left) && evaluateTree (root->right);
 
         return true;
     }
