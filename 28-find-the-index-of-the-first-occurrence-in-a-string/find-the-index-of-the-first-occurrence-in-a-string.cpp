9576ad1f1afade2b05f2eb3da61dfdc1062a7433
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-07 19:38:30 +0530

    Time: 4 ms (16.29%) | Memory: 8.5 MB (10.30%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int strStr(string haystack, string needle) {
         if(haystack.size() < needle.size()) return -1;
         unordered_set <string> s; //Create hash structure
-        s.insert(needle); //insert he needle to find in hash
+        s.insert(needle); //insert the needle to find in hash
 
         string temp = "";
         for(int i=0; i<=(haystack.length() - needle.length()) ; i++) //sliding window for needle length
