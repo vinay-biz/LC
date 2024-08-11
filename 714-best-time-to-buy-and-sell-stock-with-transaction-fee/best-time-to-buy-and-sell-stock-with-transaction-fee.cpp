1b25bab8e3cca4eea0477e493ac56751dd7f43f2
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-11 14:57:44 +0530

    Time: 174 ms (12.91%) | Memory: 99 MB (9.57%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    int helper(vector<int>& prices, int fee, int i, int curr, vector<vector<int>>& dp)
+    {
+        if(i == prices.size()) return 0;
+        if(dp[i][curr] != -1) return dp[i][curr];
+
+        int buy = 0, sell = 0;
+        if(curr == 0) buy = helper(prices, fee, i+1, 1, dp) - prices[i] - fee;
+        else sell = helper(prices, fee, i+1, 0, dp) + prices[i];
+        int none = helper(prices, fee, i+1, curr, dp);
+
+        return dp[i][curr] = max(none, max(buy,sell));
+    }
     int maxProfit(vector<int>& prices, int fee) {
-        std::ios_base::sync_with_stdio(false);
-        std::cout.tie(nullptr);
-        std::cin.tie(nullptr);
-        int curr0=0, curr1=0, ahead0=0, ahead1=0;
-        
-        for(int i = prices.size()-1; i >= 0; i--) {
-            curr0 = max(ahead1 - prices[i] - fee, ahead0);
-            curr1 = max(ahead0 + prices[i],ahead1);
-
-            ahead0 = curr0; ahead1 = curr1;
-        }
-
-        return ahead0;
+        vector<vector<int>> dp(prices.size(), vector<int> (2, -1)); 
+        return helper(prices, fee, 0, 0, dp);
     }
 };
