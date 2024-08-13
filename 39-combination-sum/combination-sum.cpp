cc22f085f10c0f6b383233663c716bfbeceb8dbc
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-14 00:04:20 +0530

    Time: 4 ms (71.72%) | Memory: 12.7 MB (52.44%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void solve(vector<int>&cand, int n, int i, int tar, vector<vector<int>>&ans, vector<int>&res){
-        if(tar == 0){
-            ans.push_back(res) ;
-            return ;
+    void helper(vector<int>& nums, int sum, int i,vector<int>& curr, vector<vector<int>>& ans)
+    {
+        if(sum == 0)
+        {
+            ans.push_back(curr);
+            return;
         }
+        if(i == nums.size()) return;
 
-        if(i == n) return ;
-
-        for(int idx = i; idx<n; idx++){
-            if(tar >= cand[idx]){
-                res.push_back(cand[idx]) ;
-                solve(cand, n, idx, tar-cand[idx], ans, res) ;
-                res.pop_back() ;
+        for(int idx = i; idx < nums.size(); ++idx)
+        {
+            if(sum >= nums[idx])
+            {
+                curr.push_back(nums[idx]);
+                helper(nums, sum-nums[idx], idx, curr, ans);
+                curr.pop_back();
             }
         }
     }
+    vector<vector<int>> combinationSum(vector<int>& nums, int sum) {
+        vector<vector<int>> ans;
+        vector<int> curr;
 
-    vector<vector<int>> combinationSum(vector<int>& cand, int tar) {
-        int n = cand.size() ;
-        vector<vector<int>>ans ;
-        vector<int>res ;
-        solve(cand, n, 0, tar, ans, res) ;
-
-        return ans ;
+        helper(nums, sum, 0, curr, ans);
+        return ans;
     }
 };
