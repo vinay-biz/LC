0e564252bef0f97616c6443fbfb727ba684215bc
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-14 14:21:44 +0530

    Time: 3 ms (31.41%) | Memory: 7.4 MB (29.00%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int helper(int n, vector<int>& dp)
-    {
-        if(n == 0 || n == 1) return 1;
-        if(n == 2) return 2;
+    int numTrees(int N) {
+        vector<int> dp(20, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
 
-        if(dp[n] != -1) return dp[n];
-
-        int ans = 0;
-        for(int i=1; i < n+1; i++)
+        for(int n = 3; n <= N; n++)
         {
-            ans += helper(i-1, dp) * helper(n - i, dp);
+            int ans = 0;
+            for(int i=1; i <= n; i++) 
+            {
+                ans = ans + dp[i-1] * dp[n-i];
+            }
+            dp[n] = ans;
         }
-        return dp[n] = ans;
-    }
-    int numTrees(int n) {
-        vector<int> dp(n+1, -1);
-        return helper(n, dp);
+        return dp[N];
     }
 };
