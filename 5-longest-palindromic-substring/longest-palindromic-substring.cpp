34bab726446725943e0b38160029cd09f7553805
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-15 22:44:34 +0530

    Time: 20 ms (65.91%) | Memory: 18.7 MB (53.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         string ans = "";
 
         for(int i = 0; i < s.size(); i++) {
