23a791bc9a0c8b8ad0c745e1250125df59f6ed17
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-16 12:21:30 +0530

    Time: 236 ms (45.75%) | Memory: 107.8 MB (69.81%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arr) {
-        int sz = arr.size();
-        int mn1 = INT_MAX, mx1 = INT_MIN, idx1 = -1;
-        int mn2 = INT_MAX, mx2 = INT_MIN, idx2 = -1;
- 
-        for(int i = 0; i < sz; i++) { //find minimal and maximal
-            if(arr[i][0] < mn1) {
-                mn1 = arr[i][0];
-                idx1 = i;
-            }
-            int n = arr[i].size() - 1;
-            if(arr[i][n] > mx2){
-                mx2 = arr[i][n];
-                idx2 = i;
-            }
+        int mn = arr[0][0];
+        int mx = arr[0].back();
+        int ans = 0;
+
+        for(int i = 1; i < arr.size(); i++)
+        {
+            ans = max(ans, abs(arr[i].back() - mn));
+            ans = max(ans, abs(mx - arr[i][0]));
+            mn = min(mn, arr[i][0]);
+            mx = max(mx, arr[i].back());
         }
-
-        for(int i = 0 ; i < sz ; i++){ //find maximal adn minimal
-            if(i != idx1) {
-                int n = arr[i].size() - 1;
-                mx1 = max(mx1, arr[i][n]);
-            }
-            if(i != idx2) {
-                mn2 = min(mn2, arr[i][0]);
-            }
-        }
-
-        return max(abs(mn1 - mx1),abs(mn2 - mx2));
-
+        return ans;
     }
 };
