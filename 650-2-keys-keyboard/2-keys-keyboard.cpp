80efbd89667906930b41e005984e61d1daac9a77
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-19 16:44:35 +0530

    Time: 2 ms (76.41%) | Memory: 7.6 MB (55.97%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int helper(int n, int currLen, int pasteLen, vector<vector<int>>& dp)
-    {
-        if(currLen == n) return 0; //Don't need any more operations
-
-        if(currLen > n) return 1001; //currLen can never exceed N
-
-        if(dp[currLen][pasteLen] != -1) return dp[currLen][pasteLen];
-
-        int cpyPaste = 2 + helper(n, currLen*2, currLen, dp);
-        int paste = 1 + helper(n, currLen + pasteLen, pasteLen, dp);
-
-        return dp[currLen][pasteLen] =min(cpyPaste, paste);
-
-    }
     int minSteps(int n) {
-        if(n == 1) return 0;
-        vector<vector<int>> dp(n+1, vector<int> (n, -1));
-        return 1 + helper(n, 1, 1, dp);
+        int ans = 0;
+        int d = 2;
+
+        while(n > 1) //We know for 1 it is 0
+        {
+            while(n%d == 0) {
+                ans += d;
+                n /= d;
+            }
+
+            d++;
+        }
+
+        return ans;
     }
 };
