268516403d6be91e777166dbf7477b7786c10399
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-21 01:39:59 +0530

    Time: 72 ms (92.58%) | Memory: 114.4 MB (48.24%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int a = INT_MAX, b = INT_MAX;
-
         for(int i=0; i<nums.size(); i++) {
             if(nums[i] <= a) a = nums[i];
             else if(nums[i] <= b) b = nums[i];
