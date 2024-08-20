1bbca4fd1a3812af61fd6e2e338724b7992707be
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-21 00:27:32 +0530

    Time: 18 ms (58.11%) | Memory: 27.2 MB (35.80%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int>& piles, int sz, int M, int index, bool turn, vector<vector<vector<int>>> &dp) {
-        if(index >= sz) return 0;
-        if(dp[turn][M][index] != -1) return  dp[turn][M][index];
+    int helper(vector<int>& piles, int sz, int M, int index, bool turn, vector<vector<vector<int>>> &dp) 
+    {
+        if(index >= sz) //If we have reached out of bounds, BASE CASE 
+            return 0;
 
-        int stones = 0 , result = turn ? -1 : INT_MAX;
+        if(dp[turn][M][index] != -1) 
+            return  dp[turn][M][index]; //DP step
 
-        for(int X = 1; X <= min(2*M, sz - index); X++) {
-            stones += piles[index + X - 1]; 
-            if(turn) result = max(result, stones + helper(piles, sz, max(M, X), index + X, !turn, dp));
-            else result = min(result, 0 + helper(piles, sz, max(M,X), index + X, !turn, dp));
+        int stones = 0 ; //Count of stones we will take
+        int result = turn ? -1 : INT_MAX; //Becase we take max in one case and min in other
+
+        for(int X = 1; X <= min(2*M, sz - index); X++)  //Make sure to add boundary check by using min
+        {
+            stones += piles[index + X - 1]; //Add the current stones 
+            if(turn) //Alice turn, we try to maximaize stones 
+                result = max(result, stones + helper(piles, sz, max(M, X), index + X, !turn, dp));
+            else //Bob's turn, we try to minimize the stones 
+                result = min(result, 0 + helper(piles, sz, max(M,X), index + X, !turn, dp));
         }
 
         return dp[turn][M][index] = result;
     }
     int stoneGameII(vector<int>& piles) {
+        //Check constraints for why
         vector<vector<vector<int>>> dp(2, vector<vector<int>> (101, vector<int>(101 , -1)));
         return helper(piles, piles.size(), 1, 0, true, dp);
     }
