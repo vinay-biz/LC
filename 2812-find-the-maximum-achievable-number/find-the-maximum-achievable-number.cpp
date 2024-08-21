9a3ff992435ab90363e19a810662e448da0d8f19
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-21 16:46:37 +0530

    Time: 3 ms (56.67%) | Memory: 9.8 MB (37.61%) - LeetSync

--- 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.cpp
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int theMaximumAchievableX(int num, int t) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         return num + t * 2;
     }
 };
