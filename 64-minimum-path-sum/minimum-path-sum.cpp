711f4ac12f3bedd3d49b98c66a2be3164987a3e7
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-24 16:21:36 +0530

    Time: 10 ms (25.20%) | Memory: 15 MB (13.93%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp)
-    {
-        if(i < 0 || j < 0) return 1e9;
-        if(i == 0 && j == 0) return grid[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        return dp[i][j] = min((grid[i][j] + helper(grid, i-1, j, dp)), (grid[i][j] + helper(grid, i, j-1, dp)));
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return helper(grid, m-1, n-1, dp);
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        dp[0][0] = grid[0][0];
+
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+
+                if(i == 0 && j == 0) continue;
+
+                int up  = (i > 0) ? grid[i][j] + dp[i-1][j] : 1e9;
+                int right = (j > 0) ? grid[i][j] + dp[i][j-1] : 1e9;
+
+                dp[i][j] = min(up, right);
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
