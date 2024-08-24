d47ca8e909e1f7eaa646a708595ec0bbe103b039
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-08-24 13:57:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (30.09%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int helper(vector<vector<int>>& arr, int m, int n, vector<vector<int>>& dp)
     {
-        if(m < 0 || n < 0 || arr[m][n] == 1) return 0;
-        if(m == 0 && n == 0) return 1;
+        if(m >= arr.size() || n >= arr[0].size() || arr[m][n] == 1) return 0;
+        if(m == arr.size()-1 && n == arr[0].size()-1) return 1;
         if(dp[m][n] != -1) return dp[m][n];
 
-        return dp[m][n] = helper(arr, m-1, n, dp) + helper(arr, m, n-1, dp);
+        return dp[m][n] = helper(arr, m+1, n, dp) + helper(arr, m, n+1, dp);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         vector<vector<int>> dp(m, vector<int> (n, -1));
-
-        return helper(obstacleGrid, m-1, n-1, dp);
+        return helper(obstacleGrid, 0, 0, dp);
     }
 };
