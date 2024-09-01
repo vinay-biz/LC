644583a902c65ad50206ac33c6b6d4839fcf1b2c
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-09-01 10:08:38 +0530

    Time: 68 ms (94.05%) | Memory: 88.1 MB (55.59%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         if(original.size() > m*n || original.size() < m*n) 
             return {};
     
