ab241b87d9e735704552d7a4d49590fb5ad087b5
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-09-02 20:07:53 +0530

    Time: 96 ms (57.54%) | Memory: 77 MB (18.08%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -6,7 +6,6 @@ public:
             sum += (long long)chalk[i];
     
         sum = k%sum;
-        if(sum == 0) return 0;
 
         for(int i = 0; i < chalk.size(); i++) {
             if(sum - chalk[i] < 0) return i;
