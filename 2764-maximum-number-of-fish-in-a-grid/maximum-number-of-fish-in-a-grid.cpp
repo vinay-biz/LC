f9a9e7697ed0514d6f81dd42e22cc8aae9e844d0
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-09-03 09:25:32 +0530

    Time: 59 ms (41.54%) | Memory: 94.4 MB (89.77%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &grid, int i, int j, int& count)
+    {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] <= 0) return;
+
+        count = count + grid[i][j];
+        grid[i][j] = -1;
+        dfs(grid,i-1,j, count);
+        dfs(grid,i+1,j, count);
+        dfs(grid,i,j-1, count);
+        dfs(grid,i,j+1, count);
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int ans = 0, m = grid.size(), n = grid[0].size();
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] > 0) {
+                    int count = 0;
+                    dfs(grid, i, j, count);
+                    ans = max(ans, count);
+                }
+            }
+        }
+        return ans;
+    }
+};
