77d8c6ac4777204bfa028725dea0bf31ad087f3e
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-09-07 15:46:12 +0530

    Time: 19 ms (27.64%) | Memory: 18 MB (24.16%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -11,6 +11,9 @@ public:
         recst.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int n = graph.size();
         vector<vector<int>> ans;
         vector<int> recst;
