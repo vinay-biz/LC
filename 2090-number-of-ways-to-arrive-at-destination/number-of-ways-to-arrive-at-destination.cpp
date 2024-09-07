c60c8e76d7799dc85932b828fdd5cbbe33ccdfd3
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-09-07 21:45:30 +0530

    Time: 63 ms (29.96%) | Memory: 37.2 MB (36.70%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<vector<long long>>> adj(n);
+        vector<vector<pair<int, long long>>> adj(n);  // Using pair instead of vector<vector<long long>>
 
-        for(int i = 0; i < roads.size(); i++){
-            long long u = (long long)roads[i][0];
-            long long v = (long long)roads[i][1];
+        for(int i = 0; i < roads.size(); i++) {
+            int u = roads[i][0];
+            int v = roads[i][1];
             long long w = (long long)roads[i][2];
 
-            adj[u].push_back({(long long)v, (long long)w});
-            adj[v].push_back({(long long)u, (long long)w});
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
         }
 
         vector<long long> dist(n, LLONG_MAX);
         vector<long long> reach(n, 0);
-        priority_queue <pair<long long,long long>, vector<pair<long long, long long>>, greater<pair<long long,long long>>> pq;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
         pq.push({0, 0}); //dist, node
-        dist[0] = (long long)0;
-        reach[0] = (long long)1;
+        dist[0] = 0;
+        reach[0] = 1;
         const int mod = 1e9 + 7;
 
         while(!pq.empty()) {
-            long long node = pq.top().second;
             long long dis = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
 
-            for(auto x : adj[node]) {
-                long long adjNode = x[0];
-                long long adjDis = x[1];
+            if (dis > dist[node]) continue;  // Skip outdated distances
 
-                if(dist[adjNode] > dis + adjDis) {
+            for(auto& [adjNode, adjDis] : adj[node]) {
+                if (dist[adjNode] > dis + adjDis) {
                     dist[adjNode] = dis + adjDis;
                     reach[adjNode] = reach[node];
                     pq.push({dist[adjNode], adjNode});
-                }
-                else if(dist[adjNode] == dis + adjDis) {
-                    reach[adjNode] = ((reach[adjNode])%mod + (reach[node])%mod)%mod;
+                } 
+                else if (dist[adjNode] == dis + adjDis) {
+                    reach[adjNode] = (reach[adjNode] + reach[node]) % mod;  // Fewer modulo operations
                 }
             }
         }
