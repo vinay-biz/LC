7a4e91a19c0e11d1afd6c95cff250de50933102f
Vinay Pandey <127542353+vinay-biz@users.noreply.github.com>
2024-09-10 08:47:49 +0530

    Time: 53 ms (27.05%) | Memory: 20 MB (21.48%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int i, vector<int>& dp) {
-        int n = nums.size();
-        if(i >= n) return 1e9;
-
-        if(i == n-1) return 0;
-
-        if(dp[i] != -1) return dp[i];
-    
-        int jump = 1e9;
-        for(int j = 1; j <= nums[i]; ++j){
-            if(i + j < n){
-                int temp = 1 + helper(nums, i+j, dp);
-                jump = min(jump, temp);
-            }
-        }
-
-        return dp[i] = jump;
-    }
     int jump(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(nums.size()+1, 1);
